01ccb43cf24cd829eabadb76768067751918aeb0
Eduardo Bouhid <eduardobouhid.neto@gmail.com>
2024-11-22 19:43:48 -0300

    Cleanup + formatting

--- orientation_model.py
+++ orientation_model.py
@@ -5,19 +5,21 @@ import pytorch_lightning as pl
 import timm
 from torch.optim.lr_scheduler import CyclicLR
 
+
 class OrientationModel(pl.LightningModule):
     def __init__(self, num_classes=4, lr=1e-3, backbone_name="resnet101"):
         super(OrientationModel, self).__init__()
         self.save_hyperparameters()
 
-        # Load ResNet34 in features_only mode, extracting feature maps from specific layers
-        self.backbone = timm.create_model(backbone_name, pretrained=True, features_only=True, out_indices=[-1])  # Extract last feature map
-        
+        self.backbone = timm.create_model(
+            backbone_name, pretrained=True, features_only=True, out_indices=[-1])  # Extract last feature map
+
         # Get the number of channels in the final feature map
         backbone_out_channels = self.backbone.feature_info[-1]['num_chs']
 
         # Adaptive pooling to make output independent of input resolution
-        self.adaptive_pool = nn.AdaptiveMaxPool2d((1, 1))  # Output: (backbone_out_channels, 1, 1)
+        # Output: (backbone_out_channels, 1, 1)
+        self.adaptive_pool = nn.AdaptiveMaxPool2d((1, 1))
 
         # Fully connected layers
         self.fc_layers = nn.Sequential(
@@ -28,16 +30,14 @@ class OrientationModel(pl.LightningModule):
             nn.Linear(128, num_classes)
         )
 
-        self.loss_fn = nn.CrossEntropyLoss() 
-        self.lr = lr  # Learning rate
+        self.loss_fn = nn.CrossEntropyLoss()
+        self.lr = lr
 
     def forward(self, x):
-        # Extract features using the ResNet34 backbone
         x = self.backbone(x)[-1]  # Get the last layer's output feature map
         x = self.adaptive_pool(x)  # Apply adaptive pooling
         x = x.view(x.size(0), -1)  # Flatten the tensor
         x = self.fc_layers(x)  # Apply fully connected layers
-        # x = torch.softmax(x, dim=1)
         return x
 
     def training_step(self, batch, batch_idx):
@@ -45,17 +45,18 @@ class OrientationModel(pl.LightningModule):
         outputs = self(images)
         loss = self.loss_fn(outputs, labels.argmax(1))
         self.log('train_loss', loss, prog_bar=True)
-        print(labels.argmax(1))
-        train_acc = (torch.softmax(outputs, dim=1).argmax(1) == labels.argmax(1)).float().mean()
+        train_acc = (torch.softmax(outputs, dim=1).argmax(1)
+                     == labels.argmax(1)).float().mean()
         self.log('train_acc', train_acc, prog_bar=True)
         return loss
-    
+
     def validation_step(self, batch, batch_idx):
         images, labels = batch
         outputs = self(images)
         loss = self.loss_fn(outputs, labels.argmax(1))
         self.log('val_loss', loss, prog_bar=False)
-        val_acc = (torch.softmax(outputs, dim=1).argmax(1) == labels.argmax(1)).float().mean()
+        val_acc = (torch.softmax(outputs, dim=1).argmax(1)
+                   == labels.argmax(1)).float().mean()
         self.log('val_acc', val_acc, prog_bar=True)
         return loss
 
@@ -65,12 +66,12 @@ class OrientationModel(pl.LightningModule):
         # Define the cyclic learning rate scheduler
         scheduler = {
             'scheduler': CyclicLR(
-                optimizer, 
+                optimizer,
                 base_lr=1e-4,       # Lower boundary of learning rate
                 max_lr=1e-2,        # Upper boundary of learning rate
                 step_size_up=2000,  # Number of iterations to increase the learning rate
-                mode='triangular2', # 'triangular2' mode, the amplitude decreases after each cycle
-                cycle_momentum=False # Disable momentum scaling (only for certain optimizers)
+                mode='triangular2',  # 'triangular2' mode, the amplitude decreases after each cycle
+                cycle_momentum=False
             ),
             'interval': 'step',    # CyclicLR should be updated every training step
             'frequency': 1,        # Frequency of scheduler update
--- test.py
+++ test.py
@@ -6,12 +6,13 @@ from albumentations.pytorch import ToTensorV2
 from dataset import OrientationDataset  # Your custom dataset class
 from orientation_model import OrientationModel  # Your model class
 
+
 def evaluate_model(model_path, assets_dir, split_ids, position, batch_size=32, num_workers=4):
     test_transforms = A.Compose([
-    A.Resize(224, 224),  # Resize to match model input
-    A.Normalize(), # Normalize the image (same as training)
-    ToTensorV2()  # Convert to PyTorch tensor
-])
+        A.Resize(224, 224),  # Resize to match model input
+        A.Normalize(),  # Normalize the image (same as training)
+        ToTensorV2()  # Convert to PyTorch tensor
+    ])
 
     test_dataset = OrientationDataset(
         assets_dir=assets_dir,
@@ -21,10 +22,12 @@ def evaluate_model(model_path, assets_dir, split_ids, position, batch_size=32, n
     )
 
     # Filter dataset by the specific position
-    test_dataset.data = [data for data in test_dataset.data if data[1] == position]
+    test_dataset.data = [
+        data for data in test_dataset.data if data[1] == position]
 
     # Create DataLoader for the test dataset
-    test_loader = DataLoader(test_dataset, batch_size=batch_size, shuffle=False, num_workers=num_workers)
+    test_loader = DataLoader(
+        test_dataset, batch_size=batch_size, shuffle=False, num_workers=num_workers)
 
     # Load the model
     model = OrientationModel.load_from_checkpoint(model_path)
@@ -46,10 +49,7 @@ def evaluate_model(model_path, assets_dir, split_ids, position, batch_size=32, n
 
             outputs = model(images)
             outputs = torch.softmax(outputs, dim=1)
-            # print("outputs:", outputs)
             predicted = torch.argmax(outputs, dim=1)
-            # print("predicted:", predicted)
-            # print("labels:", labels)
 
             total += labels.size(0)
             correct += (predicted == labels).sum().item()
@@ -59,10 +59,11 @@ def evaluate_model(model_path, assets_dir, split_ids, position, batch_size=32, n
 
     return accuracy
 
+
 if __name__ == "__main__":
-    model_path = "lightning_logs/version_11/checkpoints/epoch=3-step=196.ckpt"  # Path to your trained model
-    assets_dir = "./merged_real_clinics_assets"  # Directory containing asset folders
+    model_path = "lightning_logs/version_0/checkpoints/epoch=2-step=147.ckpt"
+    assets_dir = "./data"
     split_ids = "split_csvs/test.csv"  # Path to the test split CSV file
-    
+
     for position in ['back', 'front', 'left', 'right']:
         evaluate_model(model_path, assets_dir, split_ids, position)
